d02955bc6a081c66e74d316651064da81dd142c6
Devanshu <devanshuchi9@gmail.com>
2023-11-24 12:37:31 +0530

    Added pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full absolute top-28 left-0 right-0 flex justify-center items-center">
+      POPUP
+    </section>
+  );
 };
 
 export default Home;
