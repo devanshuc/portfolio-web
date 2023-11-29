4284df0f371e8945d6b41384167e79fbe36dd092
Devanshu <devanshuchi9@gmail.com>
2023-11-29 19:53:18 +0530

    Loaded Island w/o screenSize

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Loader from "../components/Loader";
+import Island from "../models/Island";
 
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex justify-center items-center">
@@ -9,13 +10,17 @@ import Loader from "../components/Loader";
 }
 
 const Home = () => {
+
+  const adjustIslandForDifferentDevices = ()
   return (
     <section className="w-full h-screen relative">
       <Canvas
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}>
             <Suspense fallback={<Loader />}>
+            <directionalLight />
 
+              <Island />
             </Suspense>
         </Canvas>
     </section>
