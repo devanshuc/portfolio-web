e06208535133dbb2a310502a3c7ded1fda4a6d7a
Devanshu <devanshuchi9@gmail.com>
2023-11-29 19:02:42 +0530

    Home Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
+
+{
+  /* <div className="absolute top-28 left-0 right-0 z-10 flex justify-center items-center">
+  POPUP
+</div>; */
+}
+
 const Home = () => {
   return (
-    <section className="w-full absolute top-28 left-0 right-0 flex justify-center items-center">
-      POPUP
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}>
+            <Suspense fallback={<Loader />}>
+
+            </Suspense>
+        </Canvas>
     </section>
   );
 };
