102d5b0b6917c5fc5f7c5fc8f34aa7bd10f6b04a
Devanshu <devanshuchi9@gmail.com>
2023-11-30 12:14:11 +0530

    Island Visible and Adjustable according to screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,19 +10,38 @@ import Island from "../models/Island";
 }
 
 const Home = () => {
+  const adjustIslandForDifferentDevices = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let screenRotation = [0.1, 4.7, 0];
 
-  const adjustIslandForDifferentDevices = ()
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, screenRotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForDifferentDevices();
+
   return (
     <section className="w-full h-screen relative">
       <Canvas
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}>
-            <Suspense fallback={<Loader />}>
-            <directionalLight />
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
 
-              <Island />
-            </Suspense>
-        </Canvas>
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
+        </Suspense>
+      </Canvas>
     </section>
   );
 };
