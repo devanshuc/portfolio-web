79b027f7428e35324115edb6c0ab26d48ddb7b80
Devanshu <devanshuchi9@gmail.com>
2023-11-30 12:18:43 +0530

    Fixed bit of Island Lighting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,6 +34,10 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}>
         <Suspense fallback={<Loader />}>
           <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
 
           <Island
             position={islandPosition}
