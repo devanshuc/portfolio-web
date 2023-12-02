91d16f4c546ed9b2d8e459421152c3dada792edb
Devanshu <devanshuchi9@gmail.com>
2023-12-02 17:13:15 +0530

    Handle Change and Handle Submit for Form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,30 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const Contact = () => {
+  const formRef = useRef();
   const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = () => {};
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
   const handleFocus = () => {};
 
   const handleBlur = () => {};
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+  };
+
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get In Touch Now.</h1>
-        <form className="w-full flex flex-col gap-7 mt-14">
+        <form
+          className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}>
           <label className="text-black-500 font-semibold">
             Name
             <input
